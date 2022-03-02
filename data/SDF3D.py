4a6b263335f83b13e29c137f1e29772530fae067
yxzwang <wangyuxin21@m.fudan.edu.cn>
2022-03-02 08:38:27 +0000

    SDF3D.py checking rdkit mol object

--- data/SDF3D.py
+++ data/SDF3D.py
@@ -1,5 +1,16 @@
+import enum
 from rdkit import Chem
 
 suppl = Chem.SDMolSupplier('/remote-home/yxwang/Graph/dataset/pcqm4m-v2-train.sdf')
+mol=suppl[0]
+molblock=Chem.MolToMolBlock(mol)
+print(molblock)
+molblock
+for line_number,line in enumerate(molblock.split("\n")):
+    print(line)
+    # import ipdb
+    # ipdb.set_trace()
 for idx, mol in enumerate(suppl):
     print(f'{idx}-th rdkit mol obj: {mol}')
+    import ipdb
+    ipdb.set_trace()
