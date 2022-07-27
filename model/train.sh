955e4c2ea656636c927501e19babcae6d5e6a165
JIAQING-XIE <s2001696@ed.ac.uk>
2022-07-27 22:04:41 +0100

    debug bash

--- model/train.sh
+++ model/train.sh
@@ -1,1 +1,2 @@
-python train.py --epochs 100 --checkpoint_dir "../results/checkpoint/" --save_test_dir "../results/test/" --log_dir "../results/log/" --use_pretrain False --drop_ratio 0.15 --virtual True --residual True
+#! /bin/bash
+python train.py --epochs=100 --checkpoint_dir="../results/checkpoint/" --save_test_dir="../results/test/" --log_dir="../results/log/" --drop_ratio=0.15 --virtual --residual 
